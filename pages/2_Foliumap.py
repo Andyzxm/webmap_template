65474026e1dfa0a88c80d9ec5d143b6500821ccd
Xinming Andy Zhang <andyzxm1101@gmail.com>
2024-05-02 18:54:41 -0400

    update

--- pages/2_Foliumap.py
+++ pages/2_Foliumap.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import leafmap.foliumap as leafmap
+import watergeo.foliumap as watergeo
 
 st.set_page_config(layout="wide")
 
@@ -13,14 +13,11 @@ st.sidebar.info(markdown)
 logo = "https://images-platform.99static.com/7WcMmZPzGbVHYpeaib5FcOYR314=/100x100:900x900/500x500/top/smart/99designs-contests-attachments/122/122380/attachment_122380314"
 st.sidebar.image(logo)
 
-st.title("Split-panel Map")
+st.title("watergeo map")
 
 with st.expander("See source code"):
     with st.echo():
-        m = leafmap.Map()
-        m.split_map(
-            left_layer='ESA WorldCover 2020 S2 FCC', right_layer='ESA WorldCover 2020'
-        )
-        m.add_legend(title='ESA Land Cover', builtin_legend='ESA_WorldCover')
+        m = watergeo.Map()
+        m.add_basemap("OpenTopoMap")
 
 m.to_streamlit(height=700)
